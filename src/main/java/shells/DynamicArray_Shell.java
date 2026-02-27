0d936371784b85fc926c721b346afd27bc5f14d1
DonMarioComputing <omeshmariodon@gmail.com>
2026-02-27 00:50:54 +0000

    feat: "isEmpty() method to returns a boolean if the list is currently empty  stored" docs: "Java docs for the isEmpty() method"

--- src/main/java/shells/DynamicArray_Shell.java
+++ src/main/java/shells/DynamicArray_Shell.java
@@ -6,10 +6,10 @@ public class DynamicArray_Shell {
     // todo: create a SHARED/STATIC variable to specify how much to grow the internal array by
     private static final int EXPANSION_FACTOR = 2;
 
-    private String [] data;
+    private String[] data;
     private int size;
 
-    public DynamicArray_Shell(){
+    public DynamicArray_Shell() {
         // todo: specify what size array to build
         data = new String[INITIAL_CAPACITY];
         size = 0;
@@ -36,7 +36,7 @@ public class DynamicArray_Shell {
      /**
      * Adds some values so the provided ShellTestBed can immediately show data.
      */
-    private void bootstrap(){
+    private void bootstrap() {
         // Ensure array exists
         if (data == null) {
             data = new String[INITIAL_CAPACITY];
@@ -48,183 +48,31 @@ public class DynamicArray_Shell {
         }
 
         for (int i = 0; i < 5; i++) {
-            data[i] = "Hello "+ i;
+            data[i] = "Hello " + i;
             size++;
         }
     }
 
 
-    /**
+     /**
      * Returns the size of the list.
+     *
      * @return The number of elements currently stored in the list
      */
-    public int size(){
+    public int size() {
         // todo: Add code to return the number of elements currently in the list
         return size;
     }
 
     /**
-     * Returns the element at the specified index/position in the list
-     * @param index The index/position from which to get element
-     * @return Element at specified index/position
-     * @throws IndexOutOfBoundsException if specified index/position is outside bounds of list data
-     */
-    public String get(int index){
-        // Validate for good data - Confirm the supplied position/index is valid to access
-        validateIndexForAccess(index);
-
-        // todo: Add code to return the element at the specified index
-        // return ....
-        throw new UnsupportedOperationException("Remove this line after you finish the method logic!");
-    }
-
-    /**
-     * Validate a given index to check if it's within the boundaries of the data in the array.
-     * (Note: As the internal array may not be full, the boundary is based on the number of elements currently in the
-     * list)
-     * @param index Index/position to validate as existing within this list
-     * @throws IndexOutOfBoundsException if supplied index is < 0 or >= size of list
-     */
-    private void validateIndexForAccess(int index){
-        // todo: Update the below condition to check if the position/index requested is after the end of the data
-        // Remember, the length of the array is not always the end of the data - the array might not be full!
-        if(index < 0){
-            throw new IndexOutOfBoundsException("Supplied index (" + index + ") is outside bounds of list");
-        }
-    }
-
-    /**
-     * Replaces the internal array with a bigger version if it's full.
-     */
-    private void ensureCapacity(){
-        // If the internal array is full
-        if(size == data.length){
-            // todo: Create a new array based on size of internal array * expansion factor
-            // todo: Copy all elements from internal array to new array
-            // todo: Replace internal array with new array (data = new array)
-        }
-    }
-
-    /**
-     * Validate a given String to check if it's null
-     * @param element the String to be checked
-     * @throws IllegalArgumentException if supplied String is null
-     */
-    private void validateForNull(String element){
-        // todo: Check if supplied String is null
-        // If it is, throw an exception (see JavaDoc for which type!)
-    }
-
-    /**
-     * Adds a supplied element to the end of the list.
-     * If internal array is full, it will be replaced with a larger version
-     * to facilitate adding new element. Null elements are not permitted.
+     * Checks whether the list contains no elements.
      *
-     * @param element The new element to be added to the end of the list.
-     * @throws IllegalArgumentException if the supplied element is null.
+     * @return true if size == 0, false otherwise
      */
-    public void add(String element){
-        // todo: Validate element to make sure it's not null
-
-        // Make sure there's space to add new element
-        ensureCapacity();
-
-        // todo: Insert element at position/index after data ends
-        //  (remember that position of last element in list is size-1)
-
-        // todo: Increase number of elements in list
+    public boolean isEmpty() {
+        return size == 0;
     }
 
-    /**
-     * Gets the position of the first instance of a supplied element in the list.
-     * This performs a case-insensitive linear search. Search for null is not permitted.
-     *
-     * @param element The element to be found in the list
-     * @return the index of the first matching instance found in the list, or -1 if no match is found
-     * @throws IllegalArgumentException if element to be found is null
-     */
-    public int indexOf(String element){
-        validateForNull(element);
 
-        // todo: Linear search through internal array (from 0 to end of DATA, not end of array)
-        // Check if current element matches supplied element (case-insensitive)
-        // If a match is found, return its position immediately
 
-        // If no match is found by end of data, return -1
-
-        throw new UnsupportedOperationException("Remove this line after you finish the method logic!");
-    }
-
-    public void add(int index, String element){
-        // todo: Validate element to confirm it's not null
-
-        // todo: Validate index to confirm it's within boundaries -
-        //      remember that the validation for adding is not quite the same as the
-        //      validation for getting!
-
-        // todo: make sure there's enough space for the new element
-
-        // todo: shift everything in array up by one space,
-        //  - start at the end of the *data* (not the array!), work towards specified index
-
-        // todo: add element into array at specified position
-
-        // todo: Increase size of list by 1
-    }
-
-    /**
-     * Checks if a supplied element is present within the list (case-insensitive). Search for null is not permitted.
-     * @param element The element to be found within the list.
-     * @return True if the element is located anywhere within the list, false otherwise.
-     * @throws IllegalArgumentException if supplied element is null
-     */
-    public boolean contains(String element){
-        // todo: Complete logic
-        // todo: Validate for null (throw exception if its null)
-        // todo: Search for match
-        // todo: Return verdict
-
-        throw new UnsupportedOperationException("Remove this line after you finish the method logic!");
-    }
-
-    /**
-     * Removes the element found at the specified position
-     * @param index The index/position from which to remove/delete the data
-     * @return The element removed from the specified position
-     * @throws IndexOutOfBoundsException where the supplied index is outside the boundaries of the data in the list
-     */
-    public String remove(int index){
-        // todo: Complete logic
-        // todo: Validate for illegal index (throw exception if its outside the boundaries)
-        // todo: Store copy of element at specified index
-        // todo: Shift delete element from index
-        // note: This can be done using System.arraycopy() if you want
-        // todo: Return copy of deleted element
-
-        throw new UnsupportedOperationException("Remove this line after you finish the method logic!");
-    }
-
-    /**
-     * Removes the first instance of the specified element (search is case-insensitive)
-     * @param element The element to be removed/deleted from the list
-     * @return True if the element was deleted, false if it could not be found
-     * @throws IllegalArgumentException where the element supplied is null.
-     */
-    public boolean remove(String element){
-        // todo: Complete logic
-        // todo: Validate for null (throw exception if its null)
-        // todo: Locate specified element in list
-        // todo: if not present, return false
-        // todo: Shift delete element from that position
-        // note: This can be done using System.arraycopy() if you want
-        // todo: Return true
-
-        throw new UnsupportedOperationException("Remove this line after you finish the method logic!");
-    }
-
-    // Extra methods to attempt if you're bored!
-    // todo: set - replace the element at a specified index with a supplied element. Should return replaced/lost element
-    // todo: clear - delete all elements in the list (resets list to empty)
-    // todo: isEmpty - check if the list is empty (true/false)
-    // todo: toArray - create and return a *new* array containing the list's data
 }
